f695fb73a7209213718038622bd7fb2cee5a9d51
tongbin <1020090463@qq.com>
2017-07-31 20:38:38 +0800

    123

--- src/components/newMovie.vue
+++ src/components/newMovie.vue
@@ -1,18 +1,76 @@
 <template>
-	<div>
-		
+	<div id="box">
+		<group class="group">
+			<cell title="影院热映" value="更多"></cell>
+		</group>
+		<ul id="hot">
+			<li v-for="( item,index ) in hotList"
+				
+			>
+				<img :src="item.imgM"/>
+				<span>{{item.title}}</span>
+			</li>
+		</ul>
 	</div>	
 </template>
 <script>
+	var jsonp = require("jsonp");
+	import { Cell, Group } from 'vux'
 	export default{
 		data(){
 			return {
-				
+				hotList:[],
+//				list1:[],
+//				list1:[],
 			}
+		},
+		components : {
+			Cell, 
+			Group,
+		},
+		created(){
+			var that = this;
+			jsonp('https://api.douban.com/v2/movie/in_theaters?count=6',null,function(res,data){
+				console.log(data);
+				that.hotList = data.subjects.map( (item)=>{
+					return {
+					  url: 'javascript:',
+					  imgL: item.images.large,
+					  imgM: item.images.medium,
+					  title: item.title
+					}
+				} )
+			});
 		}
 	}
 
 </script>
 <style>
-
+	ul{
+		padding: 0;
+		margin: 0;
+	}
+	li{
+		list-style: none;
+	}
+	/*weui-cells*/
+	#box .weui-cells{
+		margin: 0;
+	}
+	#hot li{
+		background-color: pink;
+		text-align: center;
+		float: left;
+		width: 33.333%;
+		padding-top: 20px;
+	}
+	
+	#hot li img{
+		width: 90px;
+		height: 130px;
+	}
+	
+	#hot span{
+		font: 12px/12px "微软雅黑";
+	}
 </style>
